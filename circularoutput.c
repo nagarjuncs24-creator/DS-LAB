e4c22d4c139c1363e17050ad0362be78d3dd0192
Nagarjun <nagarjun.cs24@bmsce.ac.in>
2025-10-16 09:47:29 +0530

    Create circularoutput.c
    Signed-off-by: Nagarjun  <nagarjun.cs24@bmsce.ac.in>

---
+++ circularoutput.c
@@ -0,0 +1,51 @@
+--- Circular Queue Operations ---
+1. Insert  2. Delete  3. Display  4. Exit
+Enter your choice: 1
+Enter element to insert: 2
+2 inserted successfully.
+
+--- Circular Queue Operations ---
+1. Insert  2. Delete  3. Display  4. Exit
+Enter your choice: 1
+Enter element to insert: 3
+3 inserted successfully.
+
+--- Circular Queue Operations ---
+1. Insert  2. Delete  3. Display  4. Exit
+Enter your choice: 1
+Enter element to insert: 4
+4 inserted successfully.
+
+--- Circular Queue Operations ---
+1. Insert  2. Delete  3. Display  4. Exit
+Enter your choice: 1
+Queue Overflow! Circular queue is full.
+
+--- Circular Queue Operations ---
+1. Insert  2. Delete  3. Display  4. Exit
+Enter your choice: 3
+Circular Queue elements: 2 3 4 
+
+--- Circular Queue Operations ---
+1. Insert  2. Delete  3. Display  4. Exit
+Enter your choice: 2
+Deleted element: 2
+
+--- Circular Queue Operations ---
+1. Insert  2. Delete  3. Display  4. Exit
+Enter your choice: 2
+Deleted element: 3
+
+--- Circular Queue Operations ---
+1. Insert  2. Delete  3. Display  4. Exit
+Enter your choice: 2
+Deleted element: 4
+
+--- Circular Queue Operations ---
+1. Insert  2. Delete  3. Display  4. Exit
+Enter your choice: 2
+Queue Underflow! Circular queue is empty.
+
+--- Circular Queue Operations ---
+1. Insert  2. Delete  3. Display  4. Exit
+Enter your choice: 4
