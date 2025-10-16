00b8f7faedf76b662f9e57f60e591b8ef02b4186
Nagarjun <nagarjun.cs24@bmsce.ac.in>
2025-10-16 09:24:08 +0530

    Create queueoutput.c
    Signed-off-by: Nagarjun  <nagarjun.cs24@bmsce.ac.in>

---
+++ queueoutput.c
@@ -0,0 +1,35 @@
+----------queue menu-----------
+1.add   2.delete        3.display       4.exit
+Enter your choice: 1
+Enter the number: 1
+
+----------queue menu-----------
+1.add   2.delete        3.display       4.exit
+Enter your choice: 1
+Enter the number: 2
+
+----------queue menu-----------
+1.add   2.delete        3.display       4.exit
+Enter your choice: 1
+Enter the number: 3
+Queue overflow
+----------queue menu-----------
+1.add   2.delete        3.display       4.exit
+Enter your choice: 3
+Queue elements are:
+1       2
+----------queue menu-----------
+1.add   2.delete        3.display       4.exit
+Enter your choice: 2
+
+----------queue menu-----------
+1.add   2.delete        3.display       4.exit
+Enter your choice: 2
+
+----------queue menu-----------
+1.add   2.delete        3.display       4.exit
+Enter your choice: 2
+Queue underflow
+----------queue menu-----------
+1.add   2.delete        3.display       4.exit
+Enter your choice: 4
